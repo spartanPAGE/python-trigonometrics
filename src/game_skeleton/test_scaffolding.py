1cc7b1b2dacff4db35776309c0df6e431328e0f8
spartanPAGE <spartanpageagain@gmail.com>
2017-02-04 20:58:46 +0100

    scaffolded game skeleton (with tests)

---
+++ src/game_skeleton/test_scaffolding.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_nothing(self):
+        self.assertEqual(True, False)
+
+if __name__ == '__main__':
+    unittest.main()
