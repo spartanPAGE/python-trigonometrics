995d68a8dcea6d6b284e57b48d39949c6a2ea133
spartanPAGE <spartanpageagain@gmail.com>
2017-02-05 19:12:53 +0100

    refactored game skeleton and added more tests

--- src/game_skeleton/scaffolding.py
+++ src/game_skeleton/scaffolding.py
@@ -5,22 +5,32 @@ class GameSkeleton:
     def __init__(self):
         self.is_running = True
 
-    def stop():
+    def stop(self):
         self.is_running = False
 
-    def fetch_events():
+    def fetch_events(self):
         return []
 
-    def consume_events(e):
+    def consume_events(self, e):
         pass
 
-    def fetch_delta_time():
+    def fetch_delta_time(self):
         return 0
 
-    def update(dt):
+    def update(self, dt):
         pass
 
-    def run():
+    def draw(self, dt):
+        pass
+
+    def stream_events(self):
+        pipe(self.fetch_events(), self.consume_events)
+
+    def stream_time(self):
+        pipe(self.fetch_delta_time(), self.update)
+        pipe(self.fetch_delta_time(), self.draw)
+
+    def run(self):
         while self.is_running:
-            pipe(self.fetch_events(), self.consume_events)
-            pipe(self.fetch_delta_time(), self.update)
+            self.stream_events()
+            self.stream_time()
--- src/game_skeleton/test_scaffolding.py
+++ src/game_skeleton/test_scaffolding.py
@@ -6,8 +6,23 @@ class GameMock(test_subject.GameSkeleton):
     def __init__(self):
         super().__init__()
 
+    def fetch_events(self):
+        return ["example event", "foo", "bar"]
+
+    def consume_events(self, events):
+        self.is_example_event_in_events = "example event" in events
+
 
 class TestGameSkeleton(unittest.TestCase):
-
     def test_is_running_right_after_creation(self):
         self.assertTrue(GameMock().is_running)
+
+    def test_is_not_running_right_after_stop(self):
+        game = GameMock()
+        game.stop()
+        self.assertFalse(game.is_running)
+
+    def test_events_are_streamed_properly(self):
+        game = GameMock()
+        game.stream_events()
+        self.assertTrue(game.is_example_event_in_events)
