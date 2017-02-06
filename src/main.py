b098ddf29927749cbced81ff3c548e9c7f4a559d
spartanPAGE <spartanpageagain@gmail.com>
2017-02-06 03:14:27 +0100

    updated main - no more stupid internet examples

--- src/main.py
+++ src/main.py
@@ -1,34 +1,20 @@
 import pygame
+import game_skeleton.pygame as game
+
+class App(game.PyGameSkeleton):
+    WINDOW_SIZE = (640,)*2
+
+    def __init__(self):
+        super().__init__(*self.WINDOW_SIZE)
+
+    def draw(self, dt):
+        self.display.fill((0, 200, 255))
+        self.display.fill((255, 0, 0), (300, 200, 150, 90))
+
 
 def main():
-    """ Set up the game and run the main game loop """
-    pygame.init()      # Prepare the pygame module for use
-    surface_sz = 480   # Desired physical surface size, in pixels.
+    with game.SystemGuard():
+        App().run()
 
-    # Create surface of (width, height), and its window.
-    main_surface = pygame.display.set_mode((surface_sz, surface_sz))
-
-    # Set up some data to describe a small rectangle and its color
-    small_rect = (300, 200, 150, 90)
-    some_color = (255, 0, 0)        # A color is a mix of (Red, Green, Blue)
-
-    while True:
-        ev = pygame.event.poll()    # Look for any event
-        if ev.type == pygame.QUIT:  # Window close button clicked?
-            break                   #   ... leave game loop
-
-        # Update your game objects and data structures here...
-
-        # We draw everything from scratch on each frame.
-        # So first fill everything with the background color
-        main_surface.fill((0, 200, 255))
-
-        # Overpaint a smaller rectangle on the main surface
-        main_surface.fill(some_color, small_rect)
-
-        # Now the surface is ready, tell pygame to display it!
-        pygame.display.flip()
-
-    pygame.quit()     # Once we leave the loop, close the window.
-
-main()
+if __name__ == "__main__":
+    main()
