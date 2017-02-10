ea8479a46c563a19d2ad2a9f68bcc1c8ac19ef3f
spartanPAGE <spartanpageagain@gmail.com>
2017-02-10 07:42:58 +0100

    refactorized point's tests

--- src/app/test_point.py
+++ src/app/test_point.py
@@ -1,33 +1,30 @@
 import unittest
-import app.point as test_subject
+import app.point as subject
 
-class PointTestCase(unittest.TestCase):
-    subject = test_subject
-
-class PointCreationCase(PointTestCase):
+class PointCreationCase(unittest.TestCase):
     def test_creation(self):
-        p = self.subject.Point(5, 6)
+        p = subject.Point(5, 6)
         self.assertEqual(p.seq, [5, 6])
         self.assertEqual(p.x, 5)
         self.assertEqual(p.y, 6)
 
-class PointArithmeticsCase(PointTestCase):
+class PointArithmeticsCase(unittest.TestCase):
     def test_addition(self):
-        p = self.subject.Point(5, 6) + self.subject.Point(-3, 4)
+        p = subject.Point(5, 6) + subject.Point(-3, 4)
         self.assertEqual(p.seq, [2, 10])
 
     def test_substraction(self):
-        p = self.subject.Point(5, 6) - self.subject.Point(-3, 4)
+        p = subject.Point(5, 6) - subject.Point(-3, 4)
         self.assertEqual(p.seq, [8, 2])
 
     def test_multiplication(self):
-        p = self.subject.Point(5, 6) * self.subject.Point(-3, 4)
+        p = subject.Point(5, 6) * subject.Point(-3, 4)
         self.assertEqual(p.seq, [-3*5, 6*4])
 
     def test_realdivision(self):
-        p = self.subject.Point(5, 6) / self.subject.Point(-3, 4)
+        p = subject.Point(5, 6) / subject.Point(-3, 4)
         self.assertEqual(p.seq, [5/(-3), 6/4])
 
     def test_floordivision(self):
-        p = self.subject.Point(5, 6) // self.subject.Point(-3, 4)
+        p = subject.Point(5, 6) // subject.Point(-3, 4)
         self.assertEqual(p.seq, [5//(-3), 6//4])
