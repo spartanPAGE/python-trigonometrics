f8be3bfd2e02c1557418ebb975d4c30550e8fc77
spartanPAGE <spartanpageagain@gmail.com>
2017-02-10 15:44:15 +0100

    added setters for pos aliases in line

--- src/app/basics/primitives/line.py
+++ src/app/basics/primitives/line.py
@@ -10,9 +10,17 @@ class Line(Primitive):
     def starting_position(self):
         return self.pos
 
+    @starting_position.setter
+    def starting_position(self, val):
+        self.pos = val
+
     @property
     def ending_position(self):
         return self.end_pos
+
+    @ending_position.setter
+    def ending_position(self, val):
+        self.end_pos = val
 
     def draw(self, dt, display):
         drawing_data = (self.color, self.starting_position.seq, self.ending_position.seq, self.thickness)
