345f9b67cf40dfac6c5f39009277cd56ed95b0ef
spartanPAGE <spartanpageagain@gmail.com>
2017-02-11 19:37:24 +0100

    fixedlengthmousepoitingline kind of works; Started Label coding

--- src/app/game.py
+++ src/app/game.py
@@ -2,22 +2,35 @@ import pygame
 import game_skeleton.pygame as pygame_skeleton
 PyGameSkeleton = pygame_skeleton.PyGameSkeleton
 Guard = pygame_skeleton.SystemGuard
+import math
 
 from app.point import Point
 from app.basics.primitives import CrossedCircle, Line
+from app.entity import Entity
 
-class MousePoitingLine(Line):
-    def __init__(self, pos, color, thickness):
+class FixedLengthMousePoitingLine(Line):
+    def __init__(self, pos, color, thickness, length):
         super().__init__(pos, pos, color, thickness)
+        self.length = length
 
     def update(self, *_):
-        p = pygame.mouse.get_pos()
-        self.ending_position = Point(*tuple(p))
+        self.ending_position = Point(*pygame.mouse.get_pos())
+        self.fix_length()
 
-class FixedLengthMousePoitingLine(MousePoitingLine):
-    def update(self, *_):
-        super().update(*_)
-        #todo;
+    def fix_length(self):
+        delta_pos = self.ending_position - self.starting_position
+        alpha = self.angle_in_rads
+        x = delta_pos.x + self.length * math.cos(alpha)
+        y = delta_pos.y + self.length * math.sin(alpha)
+        self.ending_position = Point(x, y)+self.starting_position
+
+    @property
+    def angle_in_rads(self):
+        delta_pos = self.ending_position - self.starting_position
+        return math.atan2(delta_pos.y, delta_pos.x)
+
+class Label(Entity):
+    pass
 
 class DisplayClearer:
     def __init__(self, color):
@@ -33,7 +46,7 @@ class Game(pygame_skeleton.PyGameSkeleton):
 
     def __init__(self):
         self.crossed_circle = CrossedCircle(Point(150, 150), 150, (255,)*3, (0,)*3, 3)
-        self.mouse_pointing_line = FixedLengthMousePoitingLine(Point(150, 150), (255, 0, 255), 3)
+        self.mouse_pointing_line = FixedLengthMousePoitingLine(Point(150, 150), (255, 0, 255), 3, self.crossed_circle.radius)
         super().__init__(*((self.crossed_circle.radius*2,)*2))
         pygame.display.set_caption(self.TITLE, self.ICONTITLE)
 
