98448b75f429b7f61e52506bb32ba5b948f241cb
spartanPAGE <spartanpageagain@gmail.com>
2017-02-11 19:46:38 +0100

    fixedlengthmousepoitingline finally works as intended; implemented label

--- src/app/game.py
+++ src/app/game.py
@@ -6,9 +6,9 @@ import math
 
 from app.point import Point
 from app.basics.primitives import CrossedCircle, Line
-from app.entity import Entity
+from app.basics.entity import Entity
 
-class FixedLengthMousePoitingLine(Line):
+class FixedLengthMousePointingLine(Line):
     def __init__(self, pos, color, thickness, length):
         super().__init__(pos, pos, color, thickness)
         self.length = length
@@ -20,8 +20,8 @@ class FixedLengthMousePoitingLine(Line):
     def fix_length(self):
         delta_pos = self.ending_position - self.starting_position
         alpha = self.angle_in_rads
-        x = delta_pos.x + self.length * math.cos(alpha)
-        y = delta_pos.y + self.length * math.sin(alpha)
+        x = self.length * math.cos(alpha)
+        y = self.length * math.sin(alpha)
         self.ending_position = Point(x, y)+self.starting_position
 
     @property
@@ -30,7 +30,25 @@ class FixedLengthMousePoitingLine(Line):
         return math.atan2(delta_pos.y, delta_pos.x)
 
 class Label(Entity):
-    pass
+    def __init__(self, pos, font, color, text_supplier):
+        super().__init__(pos)
+        self.font = font
+        self.color = color
+        self.text_supplier = text_supplier
+        self.text = ''
+        self.text_surface = None
+
+    def generate_text_surface(self, antialiasing_flag=1):
+        self.text_surface = self.font.render(self.text, antialiasing_flag, color)
+
+    def update(self, dt):
+        text = self.text_supplier()
+        if(text != self.text):
+            self.text = text
+            self.generate_text_surface()
+
+    def draw(self, dt, display):
+        display.blit(self.text_surface, self.pos)
 
 class DisplayClearer:
     def __init__(self, color):
@@ -46,7 +64,7 @@ class Game(pygame_skeleton.PyGameSkeleton):
 
     def __init__(self):
         self.crossed_circle = CrossedCircle(Point(150, 150), 150, (255,)*3, (0,)*3, 3)
-        self.mouse_pointing_line = FixedLengthMousePoitingLine(Point(150, 150), (255, 0, 255), 3, self.crossed_circle.radius)
+        self.mouse_pointing_line = FixedLengthMousePointingLine(Point(150, 150), (255, 0, 255), 3, self.crossed_circle.radius)
         super().__init__(*((self.crossed_circle.radius*2,)*2))
         pygame.display.set_caption(self.TITLE, self.ICONTITLE)
 
